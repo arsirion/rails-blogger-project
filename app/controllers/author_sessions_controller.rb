1a6bd99ee8f1eb29a320dd44c896bae397a315fb
Arsirion <arsirion@gmail.com>
2016-10-26 21:56:44 -0500

    tweaking to fix heroku bugs

--- app/controllers/author_sessions_controller.rb
+++ app/controllers/author_sessions_controller.rb
@@ -9,6 +9,7 @@ class AuthorSessionsController < ApplicationController
 			redirect_back_or_to(articles_path, notice: 'Logged in successfully.')
 		else
 			flash.now.alert = "Login failed."
+			redirect_back_or_to(articles_path, notice: "Sorry, something went wrong.")
 		end
 	end
 
